3a30506884136c641d9338db201d5f3871d363c0
wcc_devs <wcc_devs@WCC-LAB36-61325>
2026-02-12 21:34:10 -0300

    Add project files.

---
+++ listaLogica_simao/Program.cs
@@ -0,0 +1,100 @@
+﻿#region Nivel 1
+#region Atividade 1
+//Console.WriteLine("Insira sua primeira nota:");
+//double nota1 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Insira sua segunds nota:");
+//double nota2 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Insira sua terceira nota:");
+//double nota3 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Insira sua quarta nota:");
+//double nota4 = Convert.ToDouble(Console.ReadLine());
+//double media = (nota1 + nota2 + nota3 + nota4) /3;
+//Console.WriteLine($"Sua média é {media}.");
+#endregion
+
+#region Atividade 2
+//Console.WriteLine("Qual a sua idade?");
+//double idade = Convert.ToDouble(Console.ReadLine());
+
+//double idadeDias = idade * 365;
+
+//Console.WriteLine($"Você está vivo há aproximadamente {idadeDias} dias!");
+#endregion
+
+#region Atividade 3
+//Console.WriteLine("Insira o primeiro valor:");
+//int A = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Insira o segundo valor:");
+//int B = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine($"Valor A: {A}\nValor B: {B}");
+
+//int temp = A;
+//A = B;
+//B = temp;
+
+//Console.WriteLine("==== Troca de valores ====");
+
+//Console.WriteLine($"Valor A: {A}\nValor B: {B}");
+#endregion
+#endregion
+
+#region Nivel 2
+#region Atividade 1
+//Console.WriteLine("Insira sua idade:");
+//int idade = Convert.ToInt32(Console.ReadLine());
+
+//if (idade >= 18)
+//{
+//    Console.WriteLine("Parabéns, você é maior de idade, portanto já pode votar!");
+//}
+//else {
+//    Console.WriteLine($"Você ainda não é maior de idade, portanto ainda não pode votar!");
+//} 
+#endregion
+
+#region Atividade 2
+//Console.WriteLine("Insira um número:");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if (num == 0)
+//{
+//    Console.WriteLine("O seu número é zero, portanto ele é par!");
+//}
+//else if (num > 0 && num % 2 == 0)
+//{
+//    Console.WriteLine($"O seu número é {num}, portanto ele é par e positivo!");
+//}
+//else if (num > 0 && !(num % 2 == 0))
+//{
+//    Console.WriteLine($"O seu numero é o {num}, portanto ele é ímpar e positivo!");
+//}
+//else if (num < 0) {
+//    Console.WriteLine($"O seu número é o {num}, portanto ele é negativo!");
+//}
+
+#endregion
+
+#region Atividade 3
+//string usuario_correto = "admin";
+//string senha_correta = "12345";
+
+//Console.WriteLine("Insira o seu nome de usuário:");
+//string usuario_informado = Console.ReadLine();
+//Console.WriteLine("Insira sua senha:");
+//string senha_informada = Console.ReadLine();
+
+//if (usuario_correto == usuario_informado && senha_correta == senha_informada)
+//{
+//    Console.WriteLine("Login efetuado com sucesso!");
+//}
+//else
+//{
+//    Console.WriteLine("Usuário ou senha inválidos.");
+//}
+
+
+#endregion
+
+
+#endregion
