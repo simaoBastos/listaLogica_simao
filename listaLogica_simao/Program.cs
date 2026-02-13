47212ffc1de74908ee1f3bd5890ea191755b336f
simaoBastos <simao.b.silva@aluno.senai.br>
2026-02-13 16:15:28 -0300

    Added Nivel4.3 - Gerenciador de Tarefas Simples and added titles to the activities

--- listaLogica_simao/Program.cs
+++ listaLogica_simao/Program.cs
@@ -1,6 +1,7 @@
 ﻿#region Nivel 1 - Variáveis e Operações Básicas
 
 #region Atividade 1 - Calculadora de Média Simples
+//Console.WriteLine("=====N1A1 - Calculadora de Média Simples=====");
 //Console.WriteLine("Insira sua primeira nota:");
 //double nota1 = Convert.ToDouble(Console.ReadLine());
 //Console.WriteLine("Insira sua segunds nota:");
@@ -14,6 +15,7 @@
 #endregion
 
 #region Atividade 2 - Conversor de Idade
+//Console.WriteLine("=====N1A2 - Conversor de Idade=====");
 //Console.WriteLine("Qual a sua idade?");
 //double idade = Convert.ToDouble(Console.ReadLine());
 
@@ -23,6 +25,9 @@
 #endregion
 
 #region Atividade 3 - Troca de Valores
+//Console.WriteLine("=====N1A3 - Troca de Valores=====");
+
+
 //Console.WriteLine("Insira o primeiro valor:");
 //int A = Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine("Insira o segundo valor:");
@@ -44,6 +49,7 @@
 #region Nivel 2 - Estruturas Condicionais (if, else if, else)
 
 #region Atividade 1 - Verificador de Maioridade
+//Console.WriteLine("=====N2A1 - Verificador de Maioridade=====");
 //Console.WriteLine("Insira sua idade:");
 //int idade = Convert.ToInt32(Console.ReadLine());
 
@@ -57,6 +63,7 @@
 #endregion
 
 #region Atividade 2 - Classificador de Números
+//Console.WriteLine("=====N2A2 - Classificador de Números=====");
 //Console.WriteLine("Insira um número:");
 //int num = Convert.ToInt32(Console.ReadLine());
 
@@ -79,6 +86,7 @@
 #endregion
 
 #region Atividade 3 - Validador de Login Simples
+//Console.WriteLine("=====N2A3 - Validador de Login Simples=====");
 //string usuario_correto = "admin";
 //string senha_correta = "12345";
 
@@ -104,6 +112,7 @@
 #region Nivel 3 - Estruturas de Repetição (Loops)
 
 #region Atividade 1 - Contagem regressiva
+//Console.WriteLine("=====N3A1 - Contagem Regressiva=====");
 //Console.WriteLine("Insira um número para iniciar a contagem:");
 //int num = Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine("Contagem regressiva:");
@@ -118,6 +127,7 @@
 #endregion
 
 #region Atividade 2 - Soma de ímpares
+//Console.WriteLine("=====N3A2 - Soma de ímpares=====");
 //Console.WriteLine("Insira um número inteiro:");
 //int numBase = Convert.ToInt32(Console.ReadLine());
 //int numSoma = 0;
@@ -137,6 +147,7 @@
 #endregion
 
 #region Atividade 3 - Tabuada Personalizada
+//Console.WriteLine("=====N3A3 - Tabuada Personalizada=====");
 //Console.WriteLine("Insira um número:");
 //int num = Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine("Até que número você quer calculá-lo?");
@@ -163,6 +174,7 @@
 #region Nível 4 - Coleções (Arrays e Listas)
 
 #region Atividade 1 - Inversão de Array
+//Console.WriteLine("=====N4A1 - Inversão de Array=====");
 //string[] names = { "Joao", "Carlos", "Daniel", "Dracula", "Whatsapp" };
 //Console.WriteLine($"Array original:{string.Join(", ", names)}.");
 //Array.Reverse(names);
@@ -170,23 +182,68 @@
 #endregion
 
 #region Atividade 2 - Busca e Contagem em Lista (foreach)
-List <string> cores = new List<string> {"Azul", "Verde", "Azul", "Azul", "Amarelo", "Vermelho", };
-int contador = 0;
+//Console.WriteLine("=====N4A2 - Busca e Contagem em Lista====");
 
-Console.WriteLine("Insira uma cor (Azul, Verde ou Vermelho)");
-string cor = Console.ReadLine();
-foreach (string coresRepeticao in cores)
-{
-    if (coresRepeticao.ToLower() == cor.ToLower())
-    {
-        contador++; 
-    }
+//List <string> cores = new List<string> {"Azul", "Verde", "Azul", "Azul", "Amarelo", "Vermelho", };
+//int contador = 0;
 
-}
+//Console.WriteLine("Insira uma cor (Azul, Verde ou Vermelho)");
+//string cor = Console.ReadLine();
+//foreach (string coresRepeticao in cores)
+//{
+//    if (coresRepeticao.ToLower() == cor.ToLower())
+//    {
+//        contador++; 
+//    }
 
-Console.WriteLine($"A cor {cor} aparece {contador} vezes.");
+//}
+
+//Console.WriteLine($"A cor {cor} aparece {contador} vezes.");
 #endregion
 
+#region Atividade 3 - Gerenciador de Tarefas Simples (CRUD Básico em Memória)
+//Console.WriteLine("=====N4A3 - Gerenciador de Tarefas=====");
+//int opcao = 0;
+//List<string> tarefas = new List<string>();
+
+//while (opcao != 3) {
+//    Console.WriteLine("Escolha uma opção:\n" +
+//        "1. Adicionar Tarefa\n" +
+//        "2. Listar Tarefa\n" +
+//        "3. Sair");
+//    Console.Write("Opção: ");
+//    opcao = Convert.ToInt32(Console.ReadLine());
+
+//    Console.WriteLine(); //Pulo de linha para melhor visualização no console;
+
+//    switch (opcao)
+//    {
+//        case 1:
+//            Console.WriteLine("Insira a tarefa que você quer adicionar:");
+//            tarefas.Add(Console.ReadLine());
+//            break;
+
+//        case 2:
+//            if (tarefas == null || tarefas.Count == 0)
+//            {
+//                Console.WriteLine("Sua lista ainda está vazia, insira alguma tarefa.");
+//                break;
+//            }
+//            Console.WriteLine("Tarefas:");
+//            foreach (string tarefa in tarefas)
+//            {
+//                Console.WriteLine($"{tarefas.IndexOf(tarefa)+1} - {tarefa}");
+//            }
+//            break;
+//        case 3:
+//            Console.WriteLine("Você saiu do Gerenciador de Tarefas.");
+//            break;
+//        default:
+//            Console.WriteLine("Opcão inexistente, tente novamente.");
+//            break;
+//    }
+//    Console.WriteLine(); //Pulo de linha para melhor visualização no console;
+//}
+
 #endregion
-
-
+#endregion
